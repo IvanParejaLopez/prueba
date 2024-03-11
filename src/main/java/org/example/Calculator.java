ed30e4384db47ce6fa942b4a9e63bc16ec8988ac
IvanParejaLopez <ivanparejalopez@uma.es>
2024-03-11 10:30:56 +0100

    first commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,19 +1,1 @@
-cpackage org.example;
 
-public class Calculator {
-    import static java.lang.Math.sqrt;
-    public double squareRoot(double number) {
-        return sqrt(number) ;
-
-    }
-    public double divide(double number1, double number2) {
-        double result ;
-        if (number2 == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        result = number1/number2 ;
-        return result;
-    }
-
-
-}
